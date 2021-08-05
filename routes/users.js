37548bf0dc69c49bd5ec4e1dc3cc291bc198621c
slonevskyi <jeksons11@gmail.com>
2021-08-05 19:53:55 +0300

    Removed 500 error.

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,7 @@ router.use(bodyParser.json());
 
 router.post("/", (req, res) => {
     users.createUser(req.body, (err, user) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (user) {
+       if (user) {
             res.status(200).send(user);
         } else {
             res.status(404).send({"message": "Not found"});
@@ -23,9 +21,7 @@ router.delete("/:id", (req, res) => {
         return res.status(400).send({"message": "Delete could not work without id"});
     }
     users.deleteUserById(id, (err, user) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (user) {
+       if (user) {
             res.status(200).send(user);
         } else {
             res.status(404).send({"message": "Not found"});
@@ -39,9 +35,7 @@ router.get("/:id", (req, res) => {
         return res.status(400).send({"message": "Delete could not work without id"});
     }
     users.findUserById(id, (err, user) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (user) {
+       if (user) {
             res.status(200).send(user);
         } else {
             res.status(404).send({"message": "Not found"});
@@ -51,9 +45,7 @@ router.get("/:id", (req, res) => {
 
 router.get("/", (req, res) => {
     users.getUsers((err, user) => {
-        if (err) {
-            res.status(500).send(err);
-        } else if (user) {
+      if (user) {
             res.status(200).send(user);
         } else {
             res.status(404).send({"message": "Not found"});
